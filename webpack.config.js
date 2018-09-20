6cec3f3e8113d91824eac6331d3c8433b1566593
ymdarake <t.sakamoto.ouen@gmail.com>
2018-09-20 10:57:10 +0900

    add hand made plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,28 @@
 const path = require('path');
 
+// @see https://webpack.js.org/api/plugins/
+class ConsoleLogOnBuildWebpackPlugin {
+  apply(compiler) {
+    compiler.hooks.run.tap('ConsoleLogOnBuildWebpackPlugin', compilation => {
+      console.log('The webpack build process is starting!!!');
+    })
+    compiler.hooks.done.tap('done hook', compilation => {
+        console.log('Done!')
+    })
+    compiler.hooks.failed.tap('failed hook', compilation => {
+        console.log('Are You Nuuuuuuuuuuts!!!!?????')
+    })
+  }
+}
+
+
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugins: [
+        new ConsoleLogOnBuildWebpackPlugin()
+    ]
 };
